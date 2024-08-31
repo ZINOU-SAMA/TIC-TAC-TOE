00c88a574ffd7171ea75d611fdc4cb7ea9702cd2
Belloul Wassim Zine eddine <nw_belloul@esi.dz>
2024-08-31 16:46:36 +0100

    Setting up a roles system and a basic computer algoritm

--- script.js
+++ script.js
@@ -6,13 +6,34 @@ class Square {
     this.item.addEventListener("click", () => this.fill());
   }
   fill() {
-    if (this.filled) {
+    if (this.filled || turnIndex % 2 === 0) {
       return;
     }
     this.item.innerHTML = "<h1>X</h1>";
     this.filled = true;
+    turnIndex++;
+    roles();
   }
 }
+
+const computer = () => {
+  let flag = false;
+  while (flag === false) {
+    let random = Math.floor(Math.random() * 9);
+    if (squares[random].filled === false) {
+      squares[random].item.innerHTML = "<h1>O</h1>";
+      squares[random].filled = true;
+      flag = true;
+      turnIndex++;
+    }
+  }
+};
+
+const roles = () => {
+  if (turnIndex % 2 === 0) {
+    setTimeout(() => {computer()},2000);
+  }
+};
 
 let squares = [];
 
@@ -20,3 +41,5 @@ for (let i = 1; i <= 9; i++) {
   let square = new Square(i);
   squares.push(square);
 }
+
+let turnIndex = 1;
