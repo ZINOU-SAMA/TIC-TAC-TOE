91e75c0382347d90c98822b8554b06354807e1ff
Belloul Wassim Zine eddine <nw_belloul@esi.dz>
2024-09-03 02:36:22 +0100

    Fixed GameOver Bug

--- script.js
+++ script.js
@@ -9,15 +9,23 @@ class Square {
     if (this.filled || turnIndex % 2 === 0) {
       return;
     }
+    if (gameOver === true) {
+      return;
+    }
     this.item.innerHTML = "<h1>X</h1>";
     this.filled = true;
     turnIndex++;
     roles();
-    checkGameStatus();
+    setTimeout(() => {
+      checkGameStatus();
+    }, 200);
   }
 }
 
 const computer = () => {
+  if (gameOver === true) {
+    return;
+  }
   let flag = false;
   while (flag === false) {
     let random = Math.floor(Math.random() * 9);
@@ -26,14 +34,61 @@ const computer = () => {
       squares[random].filled = true;
       flag = true;
       turnIndex++;
-      checkGameStatus();
     }
   }
+  setTimeout(() => {
+    checkGameStatus();
+  }, 200);
 };
 
 const roles = () => {
   if (turnIndex % 2 === 0) {
-    setTimeout(() => {computer()},1500);
+    setTimeout(() => {
+      computer();
+    }, 1500);
+  }
+};
+
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+const checkForDraw = () => {
+  if (turnIndex === 10) {
+    return true;
+  }
+};
+
+const checkForWin = () => {
+  if (turnIndex >= 5) {
+    for (let combination of winningCombinations) {
+      let [a, b, c] = combination;
+      if (
+        squares[a].filled &&
+        squares[a].item.innerHTML === squares[b].item.innerHTML &&
+        squares[a].item.innerHTML === squares[c].item.innerHTML
+      ) {
+        return squares[a].item.innerHTML;
+      }
+    }
+  }
+};
+
+const checkGameStatus = () => {
+  const winner = checkForWin();
+  if (winner) {
+    alert(`${winner} Wins!`);
+    gameOver = true;
+  } else if (checkForDraw()) {
+    alert(`It's a Draw!`);
+    gameOver = true;
   }
 };
 
@@ -45,41 +100,4 @@ for (let i = 1; i <= 9; i++) {
 }
 
 let turnIndex = 1;
-
-const winningCombinations = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
-];
-
-const checkForDraw = () => {
-  if (turnIndex === 10){
-    return "Draw";
-  }
-}
-
-const checkForWin = () => {
-  if (turnIndex >= 5){
-    for (let combination of winningCombinations){
-      let [a,b,c] = combination;
-      if (squares[a].filled && squares[a].item.innerHTML === squares[b].item.innerHTML && squares[a].item.innerHTML === squares[c].item.innerHTML) {
-        return squares[a].item.innerHTML;
-      }
-    }
-  }
-}
-
-const checkGameStatus = () => {
-  const winner = checkForWin();
-  if (winner){
-    alert(`${winner} Wins!`);
-  }
-  else if (checkForDraw()){
-    alert(`It's a Draw!`);
-  }
-}
+let gameOver = false;
